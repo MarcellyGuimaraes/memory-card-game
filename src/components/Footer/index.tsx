f9f818d76dfa36540c39d3e70f62d99e564d5a89
IsisG13 <isissouzaguimaraes@gmail.com>
2022-12-08 17:30:12 -0300

    arrumando detalhe footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='flex fixed bottom-0 content-center bg-white w-full'>Desenvolvido por Marcelly Guimarães 🤗	<span className='text-xl'>Se estiver usando o celular, recomendo virar a tela 🔁</span></div>
+        <div className='flex fixed bottom-0 content-center bg-white w-full'>Desenvolvido por Marcelly Guimarães 🤗	<span className='ml-4 text-xl'>Se estiver usando o celular, recomendo virar a tela 🔁</span></div>
     )
 }
 
