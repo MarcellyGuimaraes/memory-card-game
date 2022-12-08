e08125aafdc15c4c570e4632069cf5dd57687b21
IsisG13 <isissouzaguimaraes@gmail.com>
2022-12-08 17:24:09 -0300

    arrumando detalhe footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='fixed bottom-0'>Desenvolvido por Marcelly Guimarães 🤗	<span className='text-xl'>Se estiver usando o celular, recomendo virar a tela 🔁</span></div>
+        <div className='flex fixed bottom-0 content-center bg-white w-full'>Desenvolvido por Marcelly Guimarães 🤗	<span className='text-xl'>Se estiver usando o celular, recomendo virar a tela 🔁</span></div>
     )
 }
 
